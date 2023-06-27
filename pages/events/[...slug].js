49e86181eec40a4b39cd9f465d81cc5767137a10
snkcharmer <jaryddcinco@gmail.com>
2023-06-27 17:25:33 +0800

    Add Filtered Events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,36 @@
+import EventList from "@/components/events/event-list";
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
+import ResultsTitle from "@/components/events/results-title";
+
 export default function FilteredEventPage() {
-    return (
-      <div><h1>Filtered Events</h1></div>
-    )
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
   }
-  
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (isNaN(numYear) || isNaN(numMonth)) {
+    return <p>Invalid filter, Please adjust your values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  if (!filteredEvents || filteredEvents.length == 0) {
+    return <p className="center">No events found</p>;
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
+  return (
+    <div>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </div>
+  );
+}
