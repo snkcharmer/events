afb5f9839c8c4078afc41f46d8b85caa15d9155d
snkcharmer <jaryddcinco@gmail.com>
2023-07-06 23:43:51 +0800

    Added Headers and Image components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import Button from "../ui/button";
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
+import Image from "next/image";
 
 export default function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -16,7 +17,7 @@ export default function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt="" />
+      <Image src={"/" + image} alt="" width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
